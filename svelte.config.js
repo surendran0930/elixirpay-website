83743b98c65424e60d51b2e802073c3aabd3d067
Gopinathkumaravel1996 <gopinath.kumaravel1996@gmail.com>
2026-07-27 16:14:37 +0530

    hosting name changed

--- svelte.config.js
+++ svelte.config.js
@@ -1,20 +1,22 @@
-import adapter from '@sveltejs/adapter-static';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+// import adapter from '@sveltejs/adapter-netlify';
+// import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
-	preprocess: vitePreprocess(),
+// /** @type {import('@sveltejs/kit').Config} */
+// const config = {
+// 	preprocess: vitePreprocess(),
+// 	kit: {
+// 		adapter: adapter({
+// 			edge: false,
+// 			split: false
+// 		})
+// 	}
+// };
 
+// export default config;
+import adapter from '@sveltejs/adapter-netlify';
+
+export default {
 	kit: {
-		adapter: adapter({
-			pages: 'build',
-			assets: 'build',
-			fallback: 'index.html'
-		}),
-		prerender: {
-			handleHttpError: 'warn'
-		}
+		adapter: adapter()
 	}
 };
-
-export default config;
